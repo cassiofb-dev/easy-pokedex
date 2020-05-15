9415a72c270f35c1f8215caa5466a0e2be769503
cassiofb-dev <cassiofb.souza@gmail.com>
2020-05-15 14:53:25 -0300

    more crimes

--- js/pokeinfo.js
+++ js/pokeinfo.js
@@ -7,28 +7,69 @@ const pokeinfo = {
 		if(ol) body.removeChild(ol);
 		const pokedata = await Pokedex.fetch(pokemon.url);
 		const div = document.createElement('div');
-		
-		// Conrado's mod
-		div.className += 'details';
+		div.className = 'details';
 	
+		const pokeinfo_div = document.createElement('div');
+		pokeinfo_div.className = 'container_pokeinfo';
+
+		const intro_div = document.createElement('div');
+		intro_div.className = 'container_intro';
 		const img = document.createElement('img');
 		img.src = pokedata.sprites.front_default;
-		div.append(img);
+		pokeinfo_div.append(img);
 	
+
 		const title = document.createElement('h2');
-		title.innerText = pokedata.id + ' : ' + pokedata.name;
-		div.append(title);
+		title.innerText = '#' + pokedata.id + ': ' + pokedata.name;
+		intro_div.append(title);
 	
 		const types = document.createElement('h3');
 		types.innerText = `${pokedata.types[0].type.name}`;
 		if(pokedata.types[1]) types.innerText += ` and ${pokedata.types[1].type.name}`;
-		div.append(types);
-	
-		const info = document.createElement('p');
+		intro_div.append(types);
+		pokeinfo_div.append(intro_div);
+		div.append(pokeinfo_div);
+
+		const stats_div = document.createElement('div');
+		stats_div.className = 'container_stats';
 		for(const stat of pokedata.stats) {
-			info.innerText += `| ${stat.stat.name}:${stat.base_stat} |`;
+			const info = document.createElement('p');
+			info.className = 'stats';
+			info.innerText += `${stat.stat.name}:${stat.base_stat}`;
+			stats_div.append(info);
 		}
-		div.append(info);
+		div.append(stats_div);
+
+		const abilities_div = document.createElement('div');
+		abilities_div.className = 'container_abilities';
+		for(const ability of pokedata.abilities){
+			const info = document.createElement('p');
+			info.className = 'abilities';
+			info.innerText += ability.ability.name;
+			abilities_div.append(info);
+		}
+		div.append(abilities_div);
+
+		const games_div = document.createElement('div');
+		games_div.className = 'container_games';
+		for(const ability of pokedata.game_indices){
+			const info = document.createElement('p');
+			info.className = 'games';
+			info.innerText += ability.version.name;
+			games_div.append(info);
+		}
+		div.append(games_div);
+
+		const move_div = document.createElement('div');
+		move_div.className = 'container_moves';
+		for(const move of pokedata.moves){
+			const info = document.createElement('p');
+			info.className = 'moves';
+			info.innerText += move.move.name;
+			move_div.append(info);
+		}
+		div.append(move_div);
+
 		body.append(div);
 	}
 }
